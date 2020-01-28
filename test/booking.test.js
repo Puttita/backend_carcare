af9bdc7aa0bf7527504158f2a7edd5c4018d389b
Akaporn <brombom43531@gmail.com>
2020-01-28 21:36:53 +0700

    add test booking

--- test/booking.test.js
+++ test/booking.test.js
@@ -1,36 +1,71 @@
 let assert = require('assert')
-let canBooking = require('../services/booking-service.js')
+let bookingController  = require('../controllers/booking-service')
+let mockResponse = require('mock-express-response')
+let mockRequest = require('mock-express-request')
+
 let db = require('../models')
 let sinon = require('sinon')
 let moment = require('moment')
 describe('booking', function () {
 
-    beforeEach(async function () {
-        await db.sequelize.query('PRAGMA foreign_keys = OFF')
-        await db.reservations.destroy({ where: {} })
+    beforeEach(async function () {    
+          await db.position.create({position_id: 1, position_role:'testrole'})
+          await db.employee.create({
+          employee_id: 1,
+          employee_username: 'user.mod',
+          employee_password: 'pass.out',
+          employee_fname: 'tesla',
+          employee_lname: 'edison',
+          employee_tel: '890747762',
+          position_id: 1
+        })
     })
-    it('should insert data', async function () {
-        const body = {
-            reserv_id: 1,
-            customer_name: 'surikarn',
-            license: '3กฮ 3086',
-            total_price: 280,
-            reserv_date: '2020-01-13',
-            start_date: '09:45:00',
-            end_date: '10:40:00',
-            reserv_status: 0,
-            employee_id: 0,
-            car_wash_id: 2,
-            booking_detail_id: 1,
-            total: 3,
-            time_duration: '01:15:00',
-            clean_service_id:1
-        }
-        await canBooking.insert(body)
-        let insert = await db.booking_detail.findOne({where:{reserv_id: 1}})        
-        await assert.equal(insert.reserv_id, 1, 'reserv_id should return 1')
-        await assert.equal(insert.booking_detail_id, 1, 'booking_detail_id should return `1`')
-        await assert.equal(insert.total, 3, 'booking_detail_id should return `3`')        
-        await assert.equal(insert.clean_service_id, 1, 'clean_service_id Should return `1`')
-    })
+
+    it('should return 200 and reservation data', async function(){
+      let body = {
+         reservation: {
+           reserv_id: 1,
+           customer_name: 'testname',
+           license: 'DE222',
+           total_price: 160,
+           reserv_date: await moment().format('YYYY-MM-DD'),
+           start_date: await moment().format('HH:mm:ss'),
+           end_date: await moment().add(30, 'minuts').format('HH:mm:ss'), 
+           reserv_status: 0,
+           employee_id: 1,
+           car_wash_id: 1,
+         },
+         booking_detail: [{
+           booking_detail_id: 1,
+           total: 200,
+           time_duration: '00:30:00',
+           clean_service_id: 1,
+           reserv_id: 1
+          }
+         ]
+      }
+      let req = new mockRequest({body})      
+      let res = new mockResponse({})
+      let next = function() { exec = true }
+     
+      await bookingController.book(req, res, next)
+      await assert.equal(res._getJSON().result, 'Success', 'should return success')
+      await assert.equal(typeof res._getJSON().data.reservation, 'object', 'should return object')
+      
+      const reserv = await db.reservations.findOne({where:{reserv_id: 1}})
+      await assert.equal(body.reservation.reserv_id, reserv.reserv_id, `should return ${body.reservation.reserv_id}`)
+      await assert.equal(body.reservation.customer_name, reserv.customer_name, `should return ${body.reservation.customer_name}`)
+      await assert.equal(body.reservation.license, reserv.license, `should return ${body.reservation.reserv_id}`)
+      await assert.equal(body.reservation.reserv_id, reserv.reserv_id, `should return ${body.reservation.reserv_id}`)
+   })
+
+   afterEach(async function(){
+       await db.sequelize.query('PRAGMA foreign_keys = OFF')
+       await db.employee.destroy({where:{}})
+       await db.position.destroy({where:{}})
+       await db.reservations.destroy({ where: {}})
+       await db.booking_detail.destroy({ where: {}})
+       await db.members.destroy({ where: {}})
+       await db.sequelize.query('PRAGMA foreign_keys = ON')
+  })
 })
